858e770d908d58e2d6f7d8fcec2e4b2341452db7
Kodux78k <koduxlive@gmail.com>
2025-12-26 00:03:43 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+{
+  "name": "Dual.Infodose",
+  "short_name": "Dual.Infodos",
+  "description": "",
+  "lang": "pt-BR",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#000000",
+  "orientation": "any",
+  "icons": [
+    {
+      "src": "assets/icons/icon-192.png",
+      "sizes": "any",
+      "type": "image/png"
+    }
+  ]
+}
